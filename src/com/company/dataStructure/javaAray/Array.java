c3d0d78633b015d72d919b10b3b2489b76a36bb8
Mohammad Akbor Sharif <msharif@ucmerced.edu>
2021-09-27 22:19:30 -0700

    custom array and arraylist

---
+++ src/com/company/dataStructure/javaAray/Array.java
@@ -0,0 +1,60 @@
+package com.company.dataStructure.javaAray;
+
+public class Array {
+    int arr[];
+    private int count;
+    private int size = 0;
+
+    Array(int size) {
+        this.size = size;
+        this.arr = new int[size];
+    }
+
+    public boolean isEmpty() {
+        return this.arr.length == 0;
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.print(this.arr[i] + " ");
+        }
+    }
+
+    public void push(int num) {
+        if (count != size) {
+            this.arr[count] = num;
+            count++;
+        } else {
+            System.out.println(num + " can't be inserted : index out of bound");
+        }
+
+    }
+
+    public void pop() {
+        if (!isEmpty()) {
+            int arrcpy[] = new int[arr.length - 1];
+            for (int i = 0; i < arr.length - 1; i++) {
+                arrcpy[i] = arr[i];
+            }
+            arr = arrcpy;
+            this.size = arr.length;
+            count--;
+        } else {
+            System.out.println("array is empty");
+        }
+    }
+
+    public int indexOf(int n) {
+        for (int i = 0; i < count; i++) {
+            if (arr[i] == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int size() {
+        return this.arr.length;
+    }
+    
+}
---
+++ src/com/company/dataStructure/javaAray/Main.java
@@ -0,0 +1,65 @@
+package com.company.dataStructure.javaAray;
+
+/*
+use of Array in Java
+* */
+public class Main {
+    public static void main(String[] args) {
+
+        //Custom array class
+        Array numbers = new Array(4);
+        numbers.push(1);
+        numbers.push(2);
+        numbers.push(3);
+        numbers.push(4);
+        //numbers.push(4);
+        numbers.pop();
+        //numbers.pop();
+        //numbers.pop();
+        //numbers.pop();
+        //numbers.pop();
+
+        numbers.print();
+        System.out.println();
+        System.out.println("Index of 2 is " + numbers.indexOf(2));
+        System.out.println("Index of 4 is " + numbers.indexOf(4));
+/*
+        //creating an array
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        int arr1[] = new int[8];
+
+        //inserting in an array
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = i * 2;
+        }
+        System.out.println();
+        //printing ar array
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        for (int n : arr1) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        //copying an array
+        Integer arr2[] = {1, 2, 3, 4, 5, 6, 7};
+
+        Integer arrcpy[] = new Integer[8];
+        for (int i = 0; i < arr2.length; i++) {
+            arrcpy[i] = arr2[i];
+        }
+
+        System.out.print("Copy using for loop: ");
+        for (Integer i : arrcpy) {
+            System.out.print(i + " ");
+        }
+
+        Integer arrcpy1[] = Arrays.copyOf(arr2, 8);
+        System.out.print("Copy using Arrays.copyof(): ");
+        for (Integer i : arrcpy1) {
+            System.out.print(i + " ");
+        }*/
+    }
+}
