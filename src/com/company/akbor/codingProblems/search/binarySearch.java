9940bfa2f6ed206f443ba30e5d25c0a462ad8d6c
Mohammad Akbor Sharif <msharif@ucmerced.edu>
2021-10-05 16:34:51 -0700

    binarySearch

---
+++ src/com/company/akbor/codingProblems/search/binarySearch.java
@@ -0,0 +1,38 @@
+package com.company.akbor.codingProblems.search;
+
+import java.util.Arrays;
+
+public class binarySearch {
+    public static int Search(Integer[] nums, int target, int first, int last) {
+        while (first + 1 < last) {
+            int mid = first + (last - first) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                last = mid;
+            } else {
+                first = mid;
+            }
+        }
+        //post processing
+        if (Math.abs(target - nums[last]) <= Math.abs(target - nums[first])) {
+            return last;
+        } else {
+            return first;
+        }
+    }
+
+    public static void main(String[] args) {
+        //Array must be sorted
+        Integer[] nums = {1, 2, 8, 4, 3, 5, 5, 6, 7, 9};
+        Arrays.sort(nums);
+        for (Integer num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        int first = 0;
+        int last = nums.length;
+        int index = Search(nums, 1, first, last);
+        System.out.println(index);
+    }
+}
