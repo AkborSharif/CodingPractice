8fa8f1eb3fa9547d9b6c8d0208c73095c6f870bc
Mohammad Akbor Sharif <msharif@ucmerced.edu>
2021-10-06 16:15:23 -0700

    search inseclear

---
+++ src/com/company/akbor/codingProblems/array/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package com.company.akbor.codingProblems.array;
+
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = 0;
+        while (start + 1 < end) {
+            mid = (start + end) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                start = mid;
+            } else end = mid;
+        }
+        if (target <= nums[start]) return start;
+        if (target == nums[end] || nums[start] < target && target < nums[end]) return end;
+        else return end + 1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 7));
+    }
+}
