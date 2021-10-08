8a7446c078976eda10666a904ba0283e8c35c71c
Mohammad Akbor Sharif <msharif@ucmerced.edu>
2021-10-07 22:47:49 -0700

    power of three

---
+++ src/com/company/akbor/codingProblems/number/PowerOfThree.java
@@ -0,0 +1,19 @@
+package com.company.akbor.codingProblems.number;
+
+public class PowerOfThree {
+    public static boolean isPowerOfThree(int n) {
+        if (n < 1) return false;
+        while (n % 3 == 0) {
+            n = n / 3;
+            //System.out.println(n);
+        }
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(27));
+        System.out.println(isPowerOfThree(45));
+        System.out.println(isPowerOfThree(1));
+        System.out.println(isPowerOfThree(0));
+    }
+}
