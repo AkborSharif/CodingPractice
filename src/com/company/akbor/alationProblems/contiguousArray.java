572b71f563d5542e8ff3b74fd70c7f6152c6d807
Akbor <Akbor>
2022-03-28 22:58:47 -0700

    new files

---
+++ src/com/company/akbor/alationProblems/contiguousArray.java
@@ -0,0 +1,31 @@
+package com.company.akbor.alationProblems;
+
+import java.util.HashMap;
+
+public class contiguousArray {
+    public static int findMaxLength(int[] nums){
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        counts.put(0,-1);
+        int maxLength = Integer.MIN_VALUE;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0){
+                count --;
+            }else{
+                count ++;
+            }
+            if (counts.containsKey(count)){
+                maxLength = Math.max(maxLength, i-counts.get(count));
+            }else{
+                counts.put(count, i);
+            }
+        }
+
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        //-1 0
+        System.out.println(findMaxLength(new int[]{0,1,0,1}));
+    }
+}
