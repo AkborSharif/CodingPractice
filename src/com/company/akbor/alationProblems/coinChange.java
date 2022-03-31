ab1c81038b3b3df036e584d31392bc51570dad39
Akbor <Akbor>
2022-03-30 22:46:09 -0700

    updates

---
+++ src/com/company/akbor/alationProblems/coinChange.java
@@ -0,0 +1,43 @@
+package com.company.akbor.alationProblems;
+
+import java.util.Arrays;
+
+public class coinChange {
+    public static int coinChange(int[] coins, int amount) {
+        int minLength = 0;
+        int dp [] = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+   /*
+    * i represent the amount starting from 0 to 11
+    * dp[i] stores the min length of the coins up to the amount i
+    */
+        for (int i = 0; i <= amount; i++) {
+            for (int coin: coins) {
+                if (coin>i) continue;
+                else {
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[amount]!=amount+1? dp[amount]:-1;
+    }
+
+    public static boolean coinChangeRecursion(int[] coins, int amount){
+        if(amount< 0) return false;
+        if (amount==0) return true;
+        System.out.println(Arrays.toString(coins) + " Remaining: "+ amount);
+        for(int i = 0; i<coins.length; i++){
+            if(coinChangeRecursion(coins, amount-coins[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(coinChange(new int[]{1,2,3}, 11));
+        System.out.println(coinChangeRecursion(new int[]{2}, 8));
+    }
+}
---
+++ src/com/company/akbor/alationProblems/revenueMilestone.java
@@ -0,0 +1,38 @@
+package com.company.akbor.alationProblems;
+
+import java.util.Arrays;
+
+public class revenueMilestone {
+    private static int search(int[] rev, int val) {
+        int left = 0, right = rev.length-1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (val == rev[mid]) {
+                return mid+1;
+            }else if (val > rev[mid]) {
+                left = mid+1;
+            } else {
+                right = mid;
+            }
+        }
+        return left+1;
+    }
+
+    public static int[] getMilestoneDays(int[] revenues, int[] milestones) {
+        int[] res = new int[milestones.length];
+        for (int i = 1; i < revenues.length; i++)
+            revenues[i] += revenues[i-1];
+        for (int i = 0; i < milestones.length; i++) {
+            res[i] = search(revenues, milestones[i]);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] revenues = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        int[] milestones = {100, 200, 500};
+
+        int [] ans = getMilestoneDays(revenues,milestones);
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
